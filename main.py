6460c77012c788b7802cf8794f79f645d8925df0
rede.wijaya <rede.wijaya@liquibit.io>
2025-03-06 21:31:08 +0700

    flask yang keren banget kali

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-from flask import Flask, request, render_template, jsonify
-import copy
+import uuid
+
+from flask import Flask, jsonify, render_template, request
+
+from auth.login import claim_user, login_required
+from models.user import dummy_db
+from router.auth import auth_router
+from router.user import user_router
 
 app = Flask(__name__)
+app.register_blueprint(user_router)
+app.register_blueprint(auth_router)
+
+
+@app.before_request
+def before_request():
+    print("MIDDLEWARE BEFORE REQUEST")
+    authrorization_token = request.headers.get("Authorization")
+    if authrorization_token:
+        user = claim_user(authrorization_token)
+        request.user = user
+
+
+@app.after_request
+def after_request(response):
+    # give  request id response header
+    request_id = str(uuid.uuid4())
+    response.headers["X-Request-ID"] = request_id
+
+    return response
 
 
 @app.errorhandler(404)
@@ -9,113 +35,12 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 
-# dummy_db = {
-#     "users": {
-#         "babebo@gmail.com": {
-#             "password": "123456",
-#             "first_name": "Babe",
-#             "last_name": "Bo",
-#             "full_name": "Babe Bo",
-#             "email": "babebo@gmail.com",
-#         }
-#     }
-# }
-dummy_db = {
-    "users": {
-        "babebo@gmail.com": {
-            "password": "EkoCmiwSimilikity",
-            "first_name": "babe",
-            "last_name": "bo",
-            "full_name": "babe bo",
-            "email": "babebo@gmail.com",
-        },
-        "eko@gmail.com": {
-            "password": "babekiewkiew",
-            "first_name": "eko",
-            "last_name": "aja",
-            "full_name": "eko aja",
-            "email": "eko@gmail.com",
-        },
-    }
-}
-
-
-def all_users_repository() -> dict:
-    return copy.deepcopy(dummy_db)
-
-
-def create_user_repository(email: str, user_data: dict):
-    dummy_db["users"][email] = user_data
-
-
-def get_all_users():
-    users = all_users_repository()["users"]
-    formated_responses = []
-    for email, user_data in users.items():
-        user_data.pop("password")
-        user_data.pop("first_name")
-        user_data.pop("last_name")
-        formated_responses.append(user_data)
-    return formated_responses
-
-
-def create_users(data_masuk: dict):
-    email = data_masuk.get("email")
-    password = data_masuk.get("password")
-    first_name = data_masuk.get("first_name")
-    last_name = data_masuk.get("last_name")
-    full_name = f"{first_name} {last_name}"
-    if not email:
-        return jsonify(
-            {"data": {"message": "Email is required"}, "success": False}
-        ), 400
-    if not password:
-        return jsonify(
-            {"data": {"message": "Password is required"}, "success": False}
-        ), 400
-    if not first_name:
-        return jsonify(
-            {"data": {"message": "First name is required"}, "success": False}
-        ), 400
-    if not last_name:
-        return jsonify(
-            {"data": {"message": "Last name is required"}, "success": False}
-        ), 400
-    data_masuk["full_name"] = full_name
-    create_user_repository(data_masuk.get("email"), data_masuk)
-    return jsonify(
-        {"data": {"message": f"{email} is registered"}, "success": True}
-    ), 201
+@app.route("/account", methods=["GET"])
+@login_required
+def account():
+    return jsonify({"data": {"you": request.user}, "success": True}), 200
 
 
 @app.route("/")
 def index():
     return render_template("index.html", users=dummy_db["users"])
-
-
-@app.route("/account")
-def account_api():
-    # logika
-    try:
-        # authorisasi ke vendor
-        raise Exception("Vendor not found")
-        # dari vendor mendapatkan token api
-    except Exception as e:
-        return jsonify({"data": {"message": "vendor error"}, "success": False}), 400
-
-    return jsonify({"data": {"apiKey": "alshd831uy374bgsd"}, "success": True}), 200
-
-
-@app.route("/users", methods=["GET", "POST", "PUT", "DELETE"])
-def user_api():
-    print("=" * 10, "METHOD ->", request.method, "=" * 10)
-    match request.method.lower():
-        case "get":
-            response = get_all_users()
-            return jsonify({"data": response, "success": True}), 200
-        case "post":
-            return create_users(request.json)
-        case "put":
-            pass
-        case "delete":
-            pass
---
+++ models/user.py
@@ -0,0 +1,18 @@
+dummy_db = {
+    "users": {
+        1: {
+            "password": "EkoCmiwSimilikity",
+            "first_name": "babe",
+            "last_name": "bo",
+            "full_name": "babe bo",
+            "email": "babebo@gmail.com",
+        },
+        2: {
+            "password": "babekiewkiew",
+            "first_name": "eko",
+            "last_name": "aja",
+            "full_name": "eko aja",
+            "email": "eko@gmail.com",
+        },
+    }
+}
